1dcdae7acf1fd4ac7472c7e20535fcaea579e4a1
apingali <ashwin@appsconsultants.com>
2014-05-11 17:39:53 -0600

    Plot4

--- Plot4.R
+++ Plot4.R
@@ -8,4 +8,20 @@ power$DateTime <- strptime(paste(power$Date, power$Time),format= "%d/%m/%Y %H:%M
 
 ## Create a dataframe with the subset of values for dates 2007-02-01 and 2007-02-02
 Power_Subset <- subset(power,as.Date(DateTime) >= as.Date("2007-02-01") & as.Date(DateTime) <= as.Date("2007-02-02"))
-png(filename = "Plot2.png", width = 480, height = 480, units = "px", pointsize = 12)
+png(filename = "Plot4.png", width = 480, height = 480, units = "px", pointsize = 12)
+par(mfrow=c(2,2))
+## Plot 1 - Global Active Power
+plot(Power_Subset$DateTime,Power_Subset$Global_active_power, pch=NA, xlab="", ylab="Global Active Power (kilowatts)")
+lines(Power_Subset$DateTime, Power_Subset$Global_active_power)
+## Plot 2 
+plot(Power_Subset$DateTime, Power_Subset$Voltage, ylab="Voltage", xlab="datetime", pch=NA)
+lines(Power_Subset$DateTime, Power_Subset$Voltage)
+## Plot 3
+plot(Power_Subset$DateTime, Power_Subset$Sub_metering_1, pch=NA, xlab="", ylab="Energy sub metering")
+lines(Power_Subset$DateTime, Power_Subset$Sub_metering_1)
+lines(Power_Subset$DateTime, Power_Subset$Sub_metering_2, col='red')
+lines(Power_Subset$DateTime, Power_Subset$Sub_metering_3, col='blue')
+legend('topright', c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = c(1,1,1),col = c('black', 'red', 'blue'),bty = 'n')
+
+## Plot 4
+
